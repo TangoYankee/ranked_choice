b940806770ddf842643816da45451749c0df7aaa
Timothy Miller <miller.tim108@gmail.com>
2021-01-18 20:03:20 -0500

    first bad test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
+#![allow(unused)]
+fn election(candidates: Vec<String>, votes: Vec<String>) -> String {
+    String::from("Winner")
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_election() {
+        let candidates = vec![String::from("A"), String::from("B"), String::from("C")];
+        let votes = vec![
+            String::from("Voter A"),
+            String::from("Voter B"),
+            String::from("Voter C"),
+        ];
+        let result = election(candidates, votes);
+        assert_eq!(String::from("A"), result);
     }
 }
